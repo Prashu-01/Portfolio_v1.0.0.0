c4d08b304978b05e1411ab3f7797b40d8f02ab1d
Prashu <prashuverma012@gmail.com>
2022-12-30 01:34:00 +0530

    Done!

---
+++ js/type.js
@@ -0,0 +1,8 @@
+{<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>}
+<script>
+    var typed = new Typed(".typE", {
+        strings: ["Coder", "Front-end Developer", "Videographer", "Photographer"],
+    typeSpeed: 100,
+    loop: true,
+        })
+</script>
